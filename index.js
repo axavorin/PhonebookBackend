5a30dfda7d06e5b0ca4145eded0b3c492637f651
Xavorin <bossespro53@gmail.com>
2025-07-03 19:25:14 +0600

    Part 3.22

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const db = require('./modules/persons')
 const app = express()
 
-app.use(morgan("tiny"))
+app.use(morgan('tiny'))
 app.use(express.static('dist'))
 app.use(express.json())
 
@@ -28,14 +28,14 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 app.post('/api/persons', (request, response) => {
-  const person = new db({...request.body})
+  const person = new db({ ...request.body })
   person.save().then(() => {
     response.json(person).end()
   })
 })
 
 app.get('/info', (request, response) => {
-  response.send(`<h2>Phonebook has info for ${db.length} people</h2> <h2>${Date()}</h2>`);
+  response.send(`<h2>Phonebook has info for ${db.length} people</h2> <h2>${Date()}</h2>`)
 })
 
 const errorHandler = (error, request, response, next) => {
